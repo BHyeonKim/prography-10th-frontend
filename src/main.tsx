0b8a183dac217df3927e328e5fefc9146874089b
BHyeonKim <rlaqhguse@gmail.com>
2025-02-15 21:41:54 +0900

    chore: supress biome warning in main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import '@/styles/index.css';
 import App from './App.tsx';
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
